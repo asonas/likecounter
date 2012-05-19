dad79e24d141fed18ca655412ceb7f2ea9b10a85
asonas <hzw1258@gmail.com>
2012-05-19 18:07:29 +0900

    さいしょ の こみっと

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Likecounter::Application.config.secret_token = '543c37df3e87bff1669e506274742e9531b938017f3ab25279f98fdcfc4276b23e70135e0de53504cda30817f23708d9346ea5653408b612753862f6b435d661'
